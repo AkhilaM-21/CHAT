c98a9ee88b86ec21891386b79b4fb86a85dbaa4f
AkhilaM-21 <akhila.merugu21@gmail.com>
2025-11-17 12:24:31 +0530

    deployemnt things

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,32 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import path from 'path';
 dotenv.config();
 const app=express();
+const __dirname=path.resolve();
+const PORT=process.env.PORT || 5000;
 
-const PORT=process.env.PORT || 8000;
-
+app.use(express.json()); // To parse the incoming requests with JSON payloads
 
 app.use('/api/auth',authRoutes)
 app.use ('/api/message',messageRoutes)
+
+// --- DEPLOYMENT ---
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+    
+    // For any route that is not an API route, serve the frontend's index.html file
+    app.get("*", (_,res) => {
+        res.sendFile(path.join(__dirname,'../frontend','dist','index.html'))
+    });
+} else {
+    // Provide a default route for development mode
+    app.get("/", (_, res) => {
+        res.send("API is running in development mode.");
+    });
+}
+
 app.listen(PORT,()=>{
-    console.log("Server is running on port 8000");
-})
+    console.log(`Server is running on port ${PORT}`);
+});
